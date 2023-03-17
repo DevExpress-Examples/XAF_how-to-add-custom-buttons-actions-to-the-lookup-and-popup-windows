e2966631545340b90eb24b2662b337ce796c0c2d
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-17 17:45:44 +0400

    EF

---
+++ CS/EFCore/PopupActionEF/PopupActionEF.Module/BusinessObjects/PopupActionEFDbContext.cs
@@ -0,0 +1,45 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using PopupAction.Module.BusinessObjects;
+
+namespace PopupActionEF.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class PopupActionEFContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<PopupActionEFEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new PopupActionEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class PopupActionEFDesignTimeDbContextFactory : IDesignTimeDbContextFactory<PopupActionEFEFCoreDbContext> {
+	public PopupActionEFEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<PopupActionEFEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=PopupActionEF");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new PopupActionEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(PopupActionEFContextInitializer))]
+public class PopupActionEFEFCoreDbContext : DbContext {
+	public PopupActionEFEFCoreDbContext(DbContextOptions<PopupActionEFEFCoreDbContext> options) : base(options) {
+	}
+    public DbSet<Contact> Contacts { get; set; }
+    //public DbSet<ModuleInfo> ModulesInfo { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+    }
+}
